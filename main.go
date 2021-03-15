0330a2bd201bd4bc0d3be16102ace63c3a2222ae
samsonov <i@denjs.com>
2021-03-15 19:11:21 +0300

    some fixes

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
     "github.com/pborman/getopt/v2"
     "github.com/google/goexpect"
     "github.com/howeyc/gopass"
+    "google.golang.org/grpc/codes"
 )
 
 const (
@@ -52,11 +53,13 @@ func main() {
     optCmdFile := getopt.StringLong("cmdlist", 'c', "./commands", "File with commands list")
     optTimeout := getopt.StringLong("timeout", 't',"60","Timeout in seconds")
     optPassword  := getopt.BoolLong("password",'p',"Promt for password")
+    optRun     := getopt.BoolLong("run",'r',"Run commands")
     optDebug   := getopt.BoolLong("debug",'d',"Debug mode")
 //    optLogFile := getopt.StringLong("log", 'l', "./megaconf.log", "Log file")
     getopt.Parse()
     if *optHelp { getopt.Usage(); os.Exit(0) }
     if *optVersion { fmt.Println(version); os.Exit(0) }
+    if *optRun!=true { getopt.Usage(); os.Exit(0) }
     //read files
     devices=ReadFile(*optDevFile)
     commands=ReadFile(*optCmdFile)
@@ -74,15 +77,19 @@ func main() {
     promptRE:= regexp.MustCompile("(>|#|\\$|>\\s|])$")
     passRE:= regexp.MustCompile("assword:")
     for di:=0; di<len(devices); di++ {
-	fmt.Printf("Connecting to %s, [%d/%d]\n",devices[di],di+1,len(devices))
+	fmt.Printf("\n\n##############################################\n#    Connecting to %s, [%d/%d]\n##############################################\n\n\n",devices[di],di+1,len(devices))
 	e, _, err := expect.Spawn(fmt.Sprintf("ssh -o StricthostKeyChecking=no -o CheckHostIP=no -p 22 %s",devices[di]),-1,expect.Verbose(*optDebug))
         if err != nil { log.Fatal(err) }
         defer e.Close()
-        if *optPassword==true {
-            e.Expect(passRE, timeout)
-	    e.Send(password + "\n")
+	_, _, _, err=e.ExpectSwitchCase([]expect.Caser{
+	    &expect.Case{R: passRE,S: password+"\n", T: expect.Continue(expect.NewStatus(codes.PermissionDenied, "Access denied, wrong password")), Rt: 2},
+	    &expect.Case{R: promptRE,T: expect.OK()},
+	},timeout)
+	if err!=nil {
+	    log.Fatal(err)
+	    continue
 	}
-        e.Expect(promptRE, timeout)
+	//e.Expect(promptRE, timeout)
         // run commands
         for i:=0; i<len(commands); i++ {
             e.Send(commands[i]+"\n\r")
